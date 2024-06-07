18984d644fd81155abe03c40344bcd4a800a7229
Harlequelrah <degbovimax19@gmail.com>
2024-06-07 13:07:22 +0000

    test

---
+++ Components/Migrations/20240607112059_isupdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace blazor_gestconf.Migrations
+{
+    /// <inheritdoc />
+    public partial class isupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
